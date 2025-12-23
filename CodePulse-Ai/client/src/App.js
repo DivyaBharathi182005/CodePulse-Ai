fb261fb22aa6e5939ad6ab498ee114b3067f93e0
DivyaBharathi182005 <divyabharathimohana2003@gmail.com>
2025-12-23 20:17:32 +0530

    Update App.js

--- CodePulse-Ai/client/src/App.js
+++ CodePulse-Ai/client/src/App.js
@@ -1,6 +1,5 @@
 import React, { useEffect, useState, useRef } from 'react';
 import Editor from '@monaco-editor/react';
-import './index.css';
 import { io } from 'socket.io-client';
 import ReactMarkdown from 'react-markdown';
 import { jsPDF } from "jspdf";
